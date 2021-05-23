d1d675ebb150aaa7ce3b14089ecdfc6091615a09
unknown <grknzr2609@gmail.com>
2021-05-23 16:09:19 +0300

    accordion closing issue fixed.

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ const questions = document.querySelectorAll(".question");
 questions.forEach(question => {
     question.addEventListener("click", () => {
         const item = question.parentElement;
+        const isActive = item.classList.contains("active") ? true : false;
         const listItems = document.querySelectorAll(".list-item");
         listItems.forEach(listItem => {
             listItem.classList.remove("active");
         })
-        item.classList.toggle("active");
+        if (!isActive)
+            item.classList.add("active");
     });
 });
