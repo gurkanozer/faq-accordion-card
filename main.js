c02c14bdb83203497232e8a51522845d669ff238
unknown <grknzr2609@gmail.com>
2021-05-23 14:23:23 +0300

    everything fixed

--- main.js
+++
@@ -1,8 +0,0 @@
-const questions = document.querySelectorAll(".question");
-
-questions.forEach(q => {
-    q.addEventListener("click", () => {
-        const item = q.parentElement;
-        item.classList.toggle("active");
-    })
-});
